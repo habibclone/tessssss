e0d0c75761fcff7af146bfe714047d6e1c6cde2a
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-09-06 05:59:15 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h3>Hello World!</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, exercitationem numquam! Consequuntur tempora maxime eos repellat, accusantium at numquam? Incidunt delectus sit expedita rerum quas possimus accusamus quo ipsum, tempora perspiciatis ipsa necessitatibus exercitationem odio quibusdam aliquid mollitia voluptatibus consectetur ab libero accusantium reprehenderit soluta dolores, voluptatum a? Omnis ipsam debitis praesentium nobis! Laboriosam, deleniti soluta aspernatur doloremque dolorum hic, aperiam odio iure in est nihil amet possimus, ipsum cupiditate vel quaerat? Beatae, veniam voluptates ab quaerat mollitia iusto autem officiis. Provident natus hic voluptatem perferendis magni similique alias aut, necessitatibus explicabo, cupiditate omnis iure odio fuga veniam animi est.</p>
+</body>
+</html>
