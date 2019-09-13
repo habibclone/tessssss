39b783dc316d2bd955fd8d537ff81a31694a29cc
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-09-13 09:12:17 +0700

    Mengurangi satu list link

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Coba Git Remote</title>
 </head>
+
 <body>
     <h3>Hello World!</h3>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, exercitationem numquam! Consequuntur tempora maxime eos repellat, accusantium at numquam? Incidunt delectus sit expedita rerum quas possimus accusamus quo ipsum, tempora perspiciatis ipsa necessitatibus exercitationem odio quibusdam aliquid mollitia voluptatibus consectetur ab libero accusantium reprehenderit soluta dolores, voluptatum a? Omnis ipsam debitis praesentium nobis! Laboriosam, deleniti soluta aspernatur doloremque dolorum hic, aperiam odio iure in est nihil amet possimus, ipsum cupiditate vel quaerat? Beatae, veniam voluptates ab quaerat mollitia iusto autem officiis. Provident natus hic voluptatem perferendis magni similique alias aut, necessitatibus explicabo, cupiditate omnis iure odio fuga veniam animi est.</p>
@@ -17,12 +19,11 @@
         <li>link</li>
         <li>link</li>
         <li>link</li>
-        <li>link</li>
     </ul>
     <form action="">
         <div class="form-group">
-          <label for=""></label>
-          <input type="password" class="form-control" name="" id="" placeholder="">
+            <label for=""></label>
+            <input type="password" class="form-control" name="" id="" placeholder="">
         </div>
     </form>
     <table>
@@ -32,4 +33,5 @@
         <tr>Cleaning</tr>
     </table>
 </body>
+
 </html>
